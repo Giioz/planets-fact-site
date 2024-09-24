7b8c59e7b52fcb05c7c362a9a0dae7d8ad46c27a
Giorgi Mamaladze <mamaladzegio99@gmail.com>
2024-09-24 15:40:16 +0400

    wip: burger menu funcs

--- src/components/shared/PlanetsHeader.tsx
+++ src/components/shared/PlanetsHeader.tsx
@@ -1,12 +1,30 @@
+import React, { useState } from "react";
 import { IconBurgerMenu } from "../icons/IconBurgerMenu";
 
 
-export const PlanetsHeader = () => {
+export const PlanetsHeader: React.FC = () => {
+  const [burgerValue, setBurgerValue] = useState(false)
+  const handleValueChange = (value:boolean) => {
+    setBurgerValue(value)
+  }
+  const isBurgerToggled = burgerValue;
+  console.log(isBurgerToggled)
+
   return (
-    <header className="flex justify-between p-sm items-center">
-        <h1 className="text-[1.5rem] text-white">THE PLANETS</h1>
-        <IconBurgerMenu />
-    </header>
+    <>
+      <header className="flex justify-between p-sm items-center">
+          <h1 className="text-[1.5rem] text-white font-medium">THE PLANETS</h1>
+          <IconBurgerMenu onValueChange={handleValueChange}/>
+          
+      </header>
+      {isBurgerToggled && (
+        <div className="sticky w-full h-[100vh]">
+          <nav className="w-full h-full bg-white absolute top-[0px]">
+            
+          </nav>
+        </div>
+      )}
+    </>
   )
 }
 
