005dcb709353e0efde28d939577cb1f805e66a7e
Giorgi Mamaladze <mamaladzegio99@gmail.com>
2024-09-24 17:18:40 +0400

    style : changed sections paddinginline

--- src/components/PlanetsAbout.tsx
+++ src/components/PlanetsAbout.tsx
@@ -2,7 +2,7 @@ import { WikiArrow } from "./icons/WikiArrow"
 
 export const PlanetsAbout = () => {
   return (
-    <section className="px-sm">
+    <section>
       <div className="border-b-[1px] border-t-[1px] border-[#979797] border-opacity-[20%]">
         <ul className="w-auto text-white flex px-[24px] py-[20px] max-gap-[44px] text-[12px] font-bold tracking-[2.6px] leading-[25px] justify-between">
           <li>OVERVIEW</li>
@@ -13,7 +13,7 @@ export const PlanetsAbout = () => {
       <div className="w-[111px] h-[111px] m-auto my-[98px]">
         <img src="./assets/planet-mercury.svg" alt="Mercury"/>
       </div>
-      <div className="max-w-[327px] text-center m-auto">
+      <div className="max-w-[350px] text-center m-auto px-sm">
         <h1 className="text-white text-[40px] font-medium">MERCURY</h1>
         <p className="text-white text-[11px] leading-[22px] font-normal mt-[16px] mb-[32px]">
           Mercury is the smallest planet in the Solar System and
@@ -26,7 +26,7 @@ export const PlanetsAbout = () => {
           <p className="text-white opacity-[50%] flex items-center gap-[3px] font-thin">Source : <a className="underline underline-offset-2 flex items-center gap-[5px] cursor-pointer font-medium" href="https:facebook.com">Wikipedia <WikiArrow /></a> </p>
         </div>
       </div>
-      <div className="max-w-[768px] h-auto flex flex-col gap-[8px] mt-[28px]">
+      <div className="max-w-[768px] h-auto flex flex-col gap-[8px] mt-[28px] px-sm mb-[50px]">
           <div className="h-[48px] w-full border-[1.5px] px-[24px] py-[13px] border-[#D8D8D8] border-opacity-[20%] text-white flex justify-between items-center">
             <p className="text-[8px] font-bold leading-[16px] tracking-[0.7px] opacity-[50%]">ROTATION TIME</p>
             <p className="text-[20px] font-medium tracking-[-1px]">124 DAYS</p>
