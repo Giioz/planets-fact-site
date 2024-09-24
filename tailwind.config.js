867cf6f555e94c657ae60b50254120f2a8bfeabf
Giorgi Mamaladze <mamaladzegio99@gmail.com>
2024-09-24 18:26:09 +0400

    add: planet colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,19 @@ export default {
     spacing: {
       sm: ["1.5rem"],
     },
-    extend: {},
+    
+    extend: {
+      colors: {
+        mercury: '#419EBB',
+        venus: '#EDA249',
+        earth: '#6f2ed6',
+        mars: '#D14C32',
+        jupiter: '#D83A34',
+        saturn: '#CD5120',
+        uranus: '#1ec2a4',
+        neptune: '#2d68f0',
+      },
+    },
   },
   plugins: [],
 }
