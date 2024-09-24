d3edf5f6738731d0c4b46d7ce9fbb29dc6cbfe52
Giorgi Mamaladze <mamaladzegio99@gmail.com>
2024-09-24 18:34:51 +0400

    wip: added comments for later

--- src/components/PlanetsAbout.tsx
+++ src/components/PlanetsAbout.tsx
@@ -10,6 +10,7 @@ export const PlanetsAbout = ({planet}:any) => {
           <li>SURFACE</li>
         </ul>
       </div>
+      {/* gasatania komponentad??idk */}
       <div className="min-w-[111px] max-w-[300px] h-[111px] m-auto my-[98px]">
         <img src={planet.images.planet} alt={planet.name} className="w-full h-full" />
       </div>
@@ -22,6 +23,7 @@ export const PlanetsAbout = ({planet}:any) => {
           <p className="text-white opacity-[50%] flex items-center gap-[3px] font-thin">Source : <a className="underline underline-offset-2 flex items-center gap-[5px] cursor-pointer font-medium" href={planet.overview.source}>Wikipedia <WikiArrow /></a> </p>
         </div>
       </div>
+      {/* gasatania calke komponentad */}
       <div className="max-w-[768px] h-auto flex flex-col gap-[8px] mt-[28px] px-sm mb-[50px]">
           <div className="h-[48px] w-full border-[1.5px] px-[24px] py-[13px] border-[#D8D8D8] border-opacity-[20%] text-white flex justify-between items-center">
             <p className="text-[8px] font-bold leading-[16px] tracking-[0.7px] opacity-[50%]">ROTATION TIME</p>
