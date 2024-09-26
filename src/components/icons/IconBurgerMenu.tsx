457660762a0eafbbc880f0bded0183d56f9ceef8
Giorgi Mamaladze <mamaladzegio99@gmail.com>
2024-09-26 15:45:44 +0400

    fix: attribute errors

--- src/components/icons/IconBurgerMenu.tsx
+++ src/components/icons/IconBurgerMenu.tsx
@@ -13,8 +13,8 @@ export const IconBurgerMenu = ({ onValueChange }:any) => {
       <div onClick={toggleBurger} className="w-[24px] h-[17px]">
         <svg width="32.000000" height="25.000000" viewBox="0 0 32 25" fill="none" xmlns="http://www.w3.org/2000/svg">
           <defs>
-            <filter id="filter_0_1922_dd" x="0.000000" y="0.000000" width="32.000000" height="25.000000" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-              <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+            <filter id="filter_0_1922_dd" x="0.000000" y="0.000000" width="32.000000" height="25.000000" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+              <feFlood floodOpacity="0" result="BackgroundImageFix"/>
               <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
               <feOffset dx="0" dy="4"/>
               <feGaussianBlur stdDeviation="1.33333"/>
@@ -25,12 +25,12 @@ export const IconBurgerMenu = ({ onValueChange }:any) => {
             </filter>
           </defs>
           <g filter="url(#filter_0_1922_dd)">
-            <rect id="Rectangle" x="4.000000" width="24.000000" height="3.000000" fill="#FFFFFF" fill-opacity="1.000000"/>
-            <rect id="Rectangle" x="4.000000" width="24.000000" height="3.000000" stroke="#979797" stroke-opacity="0" stroke-width="0.000000"/>
-            <rect id="Rectangle Copy" x="4.000000" y="7.000000" width="24.000000" height="3.000000" fill="#FFFFFF" fill-opacity="1.000000"/>
-            <rect id="Rectangle Copy" x="4.000000" y="7.000000" width="24.000000" height="3.000000" stroke="#979797" stroke-opacity="0" stroke-width="0.000000"/>
-            <rect id="Rectangle Copy 2" x="4.000000" y="14.000000" width="24.000000" height="3.000000" fill="#FFFFFF" fill-opacity="1.000000"/>
-            <rect id="Rectangle Copy 2" x="4.000000" y="14.000000" width="24.000000" height="3.000000" stroke="#979797" stroke-opacity="0" stroke-width="0.000000"/>
+            <rect id="Rectangle" x="4.000000" width="24.000000" height="3.000000" fill="#FFFFFF" fillOpacity="1.000000"/>
+            <rect id="Rectangle" x="4.000000" width="24.000000" height="3.000000" stroke="#979797" strokeOpacity="0" strokeWidth="0.000000"/>
+            <rect id="Rectangle Copy" x="4.000000" y="7.000000" width="24.000000" height="3.000000" fill="#FFFFFF" fillOpacity="1.000000"/>
+            <rect id="Rectangle Copy" x="4.000000" y="7.000000" width="24.000000" height="3.000000" stroke="#979797" strokeOpacity="0" strokeWidth="0.000000"/>
+            <rect id="Rectangle Copy 2" x="4.000000" y="14.000000" width="24.000000" height="3.000000" fill="#FFFFFF" fillOpacity="1.000000"/>
+            <rect id="Rectangle Copy 2" x="4.000000" y="14.000000" width="24.000000" height="3.000000" stroke="#979797" strokeOpacity="0" strokeWidth="0.000000"/>
           </g>
         </svg>
       </div>
