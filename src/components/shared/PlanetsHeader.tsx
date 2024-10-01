e0c47cacab17643f30436ab444a208eba3846995
Giorgi Mamaladze <mamaladzegio99@gmail.com>
2024-10-01 17:42:13 +0400

    style: list items, and added dynamic colors

--- src/components/shared/PlanetsHeader.tsx
+++ src/components/shared/PlanetsHeader.tsx
@@ -39,7 +39,17 @@ export const PlanetsHeader: React.FC<NavBarProps> = ({planets, onPlanetClick}) =
     setBurgerValue(value)
   }
   const isBurgerToggled = burgerValue;
-
+  
+  const liColors:any = {
+    Mercury: "bg-mercury",
+    Venus: "bg-venus",
+    Earth: "bg-earth",
+    Mars: "bg-mars",
+    Jupiter: "bg-jupiter",
+    Saturn: "bg-saturn",
+    Uranus: "bg-uranus",
+    Neptune: "bg-neptune"
+};
   return (
     <>
       <header className="flex justify-between p-sm items-center">
@@ -48,12 +58,20 @@ export const PlanetsHeader: React.FC<NavBarProps> = ({planets, onPlanetClick}) =
           
       </header>
       {isBurgerToggled && (
-        <div className="sticky w-full h-[100vh]">
-          <nav className="w-full h-full bg-white absolute top-[0px]">
+        <div className="sticky w-full h-[100vh] border-t-[1px] border-opacity-[20%] border-white">
+          <nav className="w-full h-full bg-[#070724] absolute top-[0px] p-sm">
             {planets.map((planet:any)=>(
-              <li key={planet.name} onClick={() => onPlanetClick(planet)}
-                  className="list-none "
-              > <div className="flex"><span className={`rounded-full w-[20px] h-[20px] bg-${planet.name.toLowerCase()}`}></span>{planet.name}</div></li>
+              <li 
+                  key={planet.name} 
+                  onClick={() => onPlanetClick(planet)}
+                  className="list-none border-b-[1px] border-opacity-[10%] border-white flex justify-between py-[20px] cursor-pointer"
+              > 
+                <div className="flex text-white gap-[25px]">
+                    <span className={`rounded-full w-[20px] h-[20px] ${liColors[planet.name]}`}></span>
+                    {planet.name.toUpperCase()} 
+                </div>
+                <div className="text-opacity-[40%] text-white"> &gt;</div>
+              </li>
             ))}
           </nav>
         </div>
