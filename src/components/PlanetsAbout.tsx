7d5eb580eda9a608ff6ba92f468d2e5e5aba3b77
Giorgi Mamaladze <mamaladzegio99@gmail.com>
2024-10-03 15:50:05 +0400

    feat: facts items as reusable component

--- src/components/PlanetsAbout.tsx
+++ src/components/PlanetsAbout.tsx
@@ -1,9 +1,15 @@
 import { useState } from "react";
 import { PlanetsAboutContent } from "./shared/PlanetsAboutContent";
 import { PlanetsFactCard } from "./shared/PlanetsFactCard"
+import { PlanetsInfoItems } from "./shared/PlanetsInfoItems";
 
 export const PlanetsAbout = ({planet}:any) => {
+
+  const [selectedData, setSelectedData] = useState(planet["overview"]);
+  const [activeIndex, setActiveIndex] = useState(Number);
+
   let facts = planet.facts
+
   const labels:any = {
     rotation: "Rotation Time",
     revolution: "Revolution Time",
@@ -11,36 +17,18 @@ export const PlanetsAbout = ({planet}:any) => {
     temperature: "Temperature",
   };
 
-  const [selectedData, setSelectedData] = useState(planet["overview"]);
-  const [activeIndex, setActiveIndex] = useState(Number);
-
   const handleClick = ( key : any, index: number) => {
     if(selectedData != planet[key]){
       setSelectedData(planet[key])
     }
-    console.log(planet)
     setActiveIndex(index)
   }
-  const listItems = [
-    { key: "overview", label: "OVERVIEW" },
-    { key: "structure", label: "STRUCTURE" },
-    { key: "geology", label: "SURFACE" }
-  ];
-
-  
 
   return (
     <section>
       <div className="border-b-[1px] border-t-[1px] border-[#979797] border-opacity-[20%]">
         <ul className="w-auto min-h-[50px] pt-[20px] text-white flex px-[24px]  max-gap-[44px] text-[12px] font-bold tracking-[2.6px] leading-[25px] justify-between items-center">
-          {listItems.map((item, index) => (
-            <li key={item.key} 
-                onClick={() => handleClick(item.key, index)} 
-                className={`pb-[21px] align-text-top
-                ${activeIndex === index ? ` border-${planet.name.toLowerCase()} border-b-[3px]` : ''}`}>
-              {item.label}
-            </li>
-          ))}
+          <PlanetsInfoItems planetName={planet.name} handleClik={handleClick} activeIndex={activeIndex}/>
         </ul>
       </div>
       <div className="w-full">
