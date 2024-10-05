a69e1cc4997bc351368e01cea2f9771fb00af750
Giorgi Mamaladze <mamaladzegio99@gmail.com>
2024-10-06 00:12:24 +0400

    style: responsive | feat: moved planet list in component

--- src/components/shared/PlanetsHeader.tsx
+++ src/components/shared/PlanetsHeader.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { IconBurgerMenu } from "../icons/IconBurgerMenu";
+import { PlanetsListItems } from "./PlanetsListItems";
 
 export interface Planet {
   name: string;
@@ -31,48 +32,30 @@ export interface Planet {
 export interface NavBarProps {
   planets: Planet[];
   onPlanetClick: (planet: Planet) => void;
+  isMobile: boolean;
 }
 
-export const PlanetsHeader: React.FC<NavBarProps> = ({planets, onPlanetClick}) => {
+export const PlanetsHeader: React.FC<NavBarProps> = ({planets, onPlanetClick, isMobile}) => {
   const [burgerValue, setBurgerValue] = useState(false)
   const handleValueChange = (value:boolean) => {
     setBurgerValue(value)
   }
   const isBurgerToggled = burgerValue;
-  
-  const liColors:any = {
-    Mercury: "bg-mercury",
-    Venus: "bg-venus",
-    Earth: "bg-earth",
-    Mars: "bg-mars",
-    Jupiter: "bg-jupiter",
-    Saturn: "bg-saturn",
-    Uranus: "bg-uranus",
-    Neptune: "bg-neptune"
-};
   return (
     <>
-      <header className="flex justify-between p-sm items-center">
+      <header className="flex justify-between p-sm items-center w-full md:justify-center md:flex-col">
           <h1 className="text-[1.5rem] text-white font-medium">THE PLANETS</h1>
-          <IconBurgerMenu onValueChange={handleValueChange}/>
-          
+          {isMobile && 
+            <IconBurgerMenu onValueChange={handleValueChange}/>
+          }
+          {!isMobile && (
+              <PlanetsListItems planets={planets} onPlanetClick={onPlanetClick} isMobile={isMobile}/>
+          )}
       </header>
       {isBurgerToggled && (
         <div className="sticky w-full h-[100vh] border-t-[1px] border-opacity-[20%] border-white">
           <nav className="w-full h-full bg-[#070724] absolute top-[0px] p-sm">
-            {planets.map((planet:any)=>(
-              <li 
-                  key={planet.name} 
-                  onClick={() => onPlanetClick(planet)}
-                  className="list-none border-b-[1px] border-opacity-[10%] border-white flex justify-between py-[20px] cursor-pointer"
-              > 
-                <div className="flex text-white gap-[25px]">
-                    <span className={`rounded-full w-[20px] h-[20px] ${liColors[planet.name]}`}></span>
-                    {planet.name.toUpperCase()} 
-                </div>
-                <div className="text-opacity-[40%] text-white"> &gt;</div>
-              </li>
-            ))}
+            <PlanetsListItems planets={planets} onPlanetClick={onPlanetClick} isMobile={isMobile}/>
           </nav>
         </div>
       )}
