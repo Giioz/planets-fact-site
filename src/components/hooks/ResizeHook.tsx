b2aac0ae28d934396aaf19b4db5b1d897201a2bb
Giorgi Mamaladze <mamaladzegio99@gmail.com>
2024-10-06 00:11:35 +0400

    chore

--- src/components/hooks/ResizeHook.tsx
+++ src/components/hooks/ResizeHook.tsx
@@ -8,15 +8,11 @@ export function useWindowSize() {
             setSize([window.innerWidth, window.innerHeight]);
         };
 
-        // Set up the event listener
-        window.addEventListener("resize", updateSize);
-
-        // Call updateSize initially to set the size based on the current window size
+        window.addEventListener("resize", updateSize);       
         updateSize();
 
-        // Clean up the event listener on component unmount
         return () => window.removeEventListener("resize", updateSize);
-    }, []); // The empty array means this effect runs only once when the component mounts
+    }, []); 
 
-    return size; // Return the current size
+    return size;
 }
