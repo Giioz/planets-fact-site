e7c753402d12a09e70d5ca171fa0c0df93146cca
Giorgi Mamaladze <mamaladzegio99@gmail.com>
2024-10-06 00:10:57 +0400

    feat: added hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,30 @@
 import './App.css'
 import { PlanetsAbout } from './components/PlanetsAbout'
 import { PlanetsHeader } from './components/shared/PlanetsHeader'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import data from './components/config/data.json';
+import { useWindowSize } from './components/hooks/ResizeHook';
 
 
 function App() {
   const [selectedPlanet, setSelectedPlanet] = useState(data[0]);
+  const [isMobile, setIsMobile] = useState(false)
+  const [width,height]:any = useWindowSize()
+  useEffect(() => {
+    if(width <= 768) {
+      setIsMobile(true)
+    }else if(width >= 768){
+      setIsMobile(false)
+    }
+  })
   const handlePlanetClick = (planet:any) => {
     setSelectedPlanet(planet);
-    console.log(selectedPlanet)
-
+    
   };
   return (
     <>
-      <PlanetsHeader planets={data} onPlanetClick={handlePlanetClick} />
-      <PlanetsAbout planet={selectedPlanet}/>
+      <PlanetsHeader planets={data} onPlanetClick={handlePlanetClick} isMobile={isMobile}/>
+      <PlanetsAbout planet={selectedPlanet} isMobile={isMobile}/>
     </>
   )
 }
