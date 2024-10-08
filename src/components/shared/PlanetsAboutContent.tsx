968dec5398ce797b3449bc8a8c3ee0a754d339d5
Giorgi Mamaladze <mamaladzegio99@gmail.com>
2024-10-08 19:25:14 +0400

    refactor: moved image to separated component

--- src/components/shared/PlanetsAboutContent.tsx
+++ src/components/shared/PlanetsAboutContent.tsx
@@ -1,20 +1,17 @@
 import { WikiArrow } from "../icons/WikiArrow"
 
-export const PlanetsAboutContent = ({planet, info}:any) => {
+export const PlanetsAboutContent = ({planet, listKey}:any) => {
   return (
     <>
-    <div className="min-w-[111px] max-w-[300px] h-[111px] m-auto my-[98px]">
-    <img src={planet.images.planet} alt={planet.name} className="w-full h-full" />
-    </div>
-    <div className="max-w-[350px] text-center m-auto px-sm">
-        <h1 className="text-white text-[40px] font-medium">{planet.name}</h1>
-        <p className="text-white text-[11px] leading-[22px] font-normal mt-[16px] mb-[32px]">
-        {info.content}
-        </p>
-        <div className="flex justify-center">
-        <p className="text-white opacity-[50%] flex items-center gap-[3px] font-thin">Source : <a className="underline underline-offset-2 flex items-center gap-[5px] cursor-pointer font-medium" href={info.source}>Wikipedia <WikiArrow /></a> </p>
-        </div>
-    </div>
+      <div className="max-w-[350px] text-center md:text-start md:">
+          <h1 className="text-white text-[40px] font-medium md:text-[48px]">{planet.name}</h1>
+          <p className="text-white text-[11px] leading-[22px] font-normal mt-[16px] mb-[32px]">
+          {planet[listKey].content}
+          </p>
+          <div className="flex justify-center min-w-[] md:justify-start">
+            <p className="text-white opacity-[50%] flex items-center gap-[3px] font-thin">Source : <a className="underline underline-offset-2 flex items-center gap-[5px] cursor-pointer font-medium" href={planet[listKey].source}>Wikipedia <WikiArrow /></a> </p>
+          </div>
+      </div>
     </>
 
   )
